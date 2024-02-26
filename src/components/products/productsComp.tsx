79b62ec2e9cc5f16ec971cbb35c2e678c442d4be
unknown <Md.Jannatul Ferdous (Emon)>
2024-02-26 12:50:48 +0600

    single product hover effect is done

--- src/components/products/productsComp.tsx
+++ src/components/products/productsComp.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { ProductProps } from "../../../type";
 import Image from "next/image";
+import { HiShoppingCart } from "react-icons/hi";
+import { FaHeart } from "react-icons/fa";
 
 const ProductsComp = ({ productData }: any) => {
   console.log("🚀 ~ ProductsComp ~ productData:", productData);
@@ -9,7 +11,7 @@ const ProductsComp = ({ productData }: any) => {
       {productData.map((product: ProductProps, i: number) => (
         <div
           key={i}
-          className="bg-white w-full text-black p-4 rounded group overflow-hidden"
+          className="bg-white w-full text-black p-4 rounded-lg group overflow-hidden"
         >
           <div className="w-full h-[260px] relative">
             <Image
@@ -17,8 +19,16 @@ const ProductsComp = ({ productData }: any) => {
               src={product.image}
               alt="productImage"
               height={300}
-              width={250}
+              width={300}
             />
+            <div className="h-20 w-12 absolute right-0 bottom-10 border-[1px] border-gray-400 rounded-md bg-white flex flex-col translate-x-20 group-hover:translate-x-0 duration-300">
+              <span className="h-full w-full text-xl border-b-[1px] border-b-gray-400 flex items-center justify-center bg-transparent hover:bg-amazon_yellow cursor-pointer">
+                <HiShoppingCart />
+              </span>
+              <span className="h-full w-full text-xl flex items-center justify-center bg-transparent hover:bg-amazon_yellow cursor-pointer">
+                <FaHeart />
+              </span>
+            </div>
           </div>
         </div>
       ))}
